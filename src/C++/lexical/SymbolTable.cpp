6baa877898913fda3b60b0cd596132ab10b7294f
Aluno <bernard_menezes@hotmail.com>
2017-08-08 16:34:55 -0300

    Adiciona enum

--- src/C++/lexical/SymbolTable.cpp
+++ src/C++/lexical/SymbolTable.cpp
@@ -1,8 +1,48 @@
 #include "SymbolTable.h"
 
 SymbolTable::SymbolTable() {
-    // FIXME: Add the tokens here.
-    // m_st["???"] = ???;
+
+    m_st["="] = TokenType.ASSIGN;
+    m_st[";"] = TokenType.DOT_COMMA;
+    m_st["."] = TokenType.COMMA;
+    m_st["["] = TokenType.OPEN_CUR;
+    m_st["]"] = TokenType.CLOSE_CUR;
+    m_st["{"] = TokenType.OPEN_BRA;
+    m_st["}"] = TokenType.CLOSE_BRA;
+    m_st["("] = TokenType.OPEN_PAR;
+    m_st[")"] = TokenType.CLOSE_PAR;
+    m_st["=>"] = TokenType.BIND;
+
+    m_st["print"] = TokenType.PRINT;
+    m_st["println"] = TokenType.PRINTLN;
+    m_st["push"] = TokenType.PUSH;
+    m_st["unshift"] = TokenType.UNSHIFT;
+    m_st["if"] = TokenType.IF;
+    m_st["else"] = TokenType.ELSE;
+    m_st["while"] = TokenType.WHILE;
+    m_st["do"] = TokenType.DO;
+    m_st["foreach"] = TokenType.FOREACH;
+    m_st["and"] = TokenType.AND;
+    m_st["or"] = TokenType.OR;
+    m_st["input"] = TokenType.INPUT;
+    m_st["size"] = TokenType.SIZE;
+    m_st["sort"] = TokenType.SORT;
+    m_st["reverse"] = TokenType.REVERSE;
+    m_st["keys"] = TokenType.KEYS;
+    m_st["values"] = TokenType.VALUES;
+
+    m_st["=="] = TokenType.EQUAL;
+    m_st["!="] = TokenType.DIFF;
+    m_st["<"] = TokenType.LESS;
+    m_st[">"] = TokenType.GREATER;
+    m_st["<="] = TokenType.LESS_EQ;
+    m_st[">="] = TokenType.GREATER_EQ;
+    m_st["."] = TokenType.CONCATENATE;
+    m_st["+"] = TokenType.PLUS;
+    m_st["-"] = TokenType.MINUS;
+    m_st["*"] = TokenType.TIMES;
+    m_st["/"] = TokenType.DIV;
+    m_st["%"] = TokenType.MOD;
 }
 
 SymbolTable::~SymbolTable() {
--- src/C++/lexical/TokenType.h
+++ src/C++/lexical/TokenType.h
@@ -7,17 +7,58 @@ enum TokenType {
     UNEXPECTED_EOF = -1,
     END_OF_FILE    = 0,
 
-    // symbols
-    // ...
+    // symbols 
+    ASSIGN = 1,              // =
+    DOT_COMMA = 2,           // ;
+    COMMA = 3,               // ,
+    OPEN_CUR = 4,            // [
+    CLOSE_CUR = 5,           // ]
+    OPEN_BRA = 6,            // {
+    CLOSE_BRA = 7,           // }
+    OPEN_PAR = 8,            // (
+    CLOSE_PAR = 9,           // )
+    BIND = 10,               // => 
 
     // keywords
-    // ...
+    PRINT = 11,               // print
+    PRINTLN = 12,             // println
+    PUSH = 13,                // push
+    UNSHIFT = 14,             // unshift
+    IF = 15,                  // if
+    ELSE = 16,                // else
+    WHILE = 17,               // while
+    DO = 18,                  // do
+    FOREACH = 19,             // foreach
+    AND = 20,                 // and
+    OR = 21,                  // or
+    INPUT = 22,               // input
+    SIZE = 23,                // size
+    SORT = 24,                // sort
+    REVERSE = 25,             // reverse
+    KEYS = 26,                // keys
+    VALUES = 27,              // values
 
     // operators
-    // ...
+    EQUAL = 28,               // ==
+    DIFF = 29,                // !=
+    LESS = 30,                // <
+    GREATER = 31,             // >
+    LESS_EQ = 32,             // <=
+    GREATER_EQ = 33,          // >=
+    CONCATENATE = 34,         // .
+    PLUS = 35,                // +
+    MINUS = 36,               // -
+    TIMES = 37,               // *
+    DIV = 38,                 // /
+    MOD = 39,                 // %
 
     // others
-    // ...
+    NUMBER = 40,              // $
+    STRING = 41,              // ""
+    SVAR = 42,
+    LVAR = 43,
+    HVAR = 44,
+
 };
 
 #endif
